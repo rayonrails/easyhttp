088e02b5699d258e7403fcb6f01297f44146b2a3
Ray Rogers <ray@rayonrails.com>
2018-04-25 18:48:06 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const http = new easyHTTP;
+
+// GET Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, response){
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Get Single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, response){
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Create Data
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post',
+};
+
+// Create Post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, response){
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Creat Put
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, response){
+//    if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   } 
+// });
+
+// Delete Post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response){
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(response);
+  }
+});
